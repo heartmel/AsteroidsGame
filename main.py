0cefb753d191328a5b345b9ad708936e412780b4
heartmel <meeliizaaa@gmail.com>
2025-09-21 20:47:17 -0700

    Add Player class and integrate with the game loop

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    while True: 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0,0,0))
+        updatable.update(dt)
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
